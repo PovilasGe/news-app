801600c460b5985d114cdf0658a2b7bc04e4350e
PovilasGe <povilasgedminas@gmail.com>
2023-02-21 12:54:54 +0200

    implement stepzen

---
+++ lib/fetchNews.ts
@@ -0,0 +1,17 @@
+const fetchNews = async (
+    category?: Category | string,
+    keywords?: string,
+    isDynamic?: boolean
+) => {
+    //graphQL query
+
+    //Fetch function with Next.js 13 cashing
+
+    //sort function by images vs not images present
+
+    //return res
+}
+
+export default fetchNews
+
+// stepzen import curl "http://api.mediastack.com/v1/news?access_key=3e30c896f6495428866a792092340867&countries=us%2Cgb&limit=100&offset=0&sort=published_desc"
